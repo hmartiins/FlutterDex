01d8d4046d6845dd213b39516c9ee3a55fba6469
Henrique Martins <hmartins224@gmail.com>
2020-07-31 21:28:53 -0300

    Adjusting the background image with Postioned

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -4,11 +4,30 @@ import 'package:flutterdex/consts/consts.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: Colors.white,
       body: Stack(
+        alignment: Alignment.topCenter,
+        overflow: Overflow.visible,
         children: <Widget>[
-          Image.asset(ConstsApp.whitePokeball),
+          Positioned(
+            top: -(160 / 4.5),
+            left: (screenWidth - (160 / 1.6)),
+            child: Opacity(
+              child: Image.asset(
+                ConstsApp.whitePokeball,
+                height: 160,
+                width: 160,
+              ),
+              opacity: 0.2,
+            ),
+          ),
+          Container(
+            child: Column(
+              children: <Widget>[],
+            ),
+          ),
         ],
       ),
     );
